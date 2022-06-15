456ed5e32931a358abf04d127f08ade7c4590515
Luuk4mst3l <l.e.amstel@gmail.com>
2022-06-15 10:02:59 +0200

    Added controllers

---
+++ app/Http/Controllers/GebruikerController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Gebruiker;
+
+class GebruikerController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/MoeilijkheidsgraadController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Moeilijkheidsgraad;
+
+
+class MoeilijkheidsgraadController extends Controller
+{
+    //
+}
